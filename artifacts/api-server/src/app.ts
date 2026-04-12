bdd469b385ff663a11cc8e8dc6fc27450bebb5b1
riss <93233740+risscode@users.noreply.github.com>
2026-04-12 20:42:41 +0700

    Refactor request and response serializers in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,10 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttpImport from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
+
+const pinoHttp = pinoHttpImport.default;
 
 const app: Express = express();
 
@@ -10,14 +12,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: Request) {
         return {
-          id: req.id,
+          id: (req as any).id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +27,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
